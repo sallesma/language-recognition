98c8b516990838c996b93da5fa049e8685983112
Martin Salles <salles.martin@gmail.com>
2015-12-16 20:42:42 +0100

    training in memory and incremental

--- app/services/trainer.rb
+++ app/services/trainer.rb
@@ -1,43 +1,63 @@
 class Trainer
 
-    def self.train(text, locale)
-        self.clear_language(locale)
-        words = TextPreprocessor.prepare(text)
-        self.probabilities(words, locale)
-    end
-
-    # TODO refactor/delete when training is incremental
-    def self.clear_language(locale)
-        FirstLetter.where(locale: locale).destroy_all
-        ChainedLetter.where(locale: locale).destroy_all
-    end
-
     # Using all words in the set of words, calculate the
     # initial probabilities and the transition probabilities
     # initial P (x) = (occurences of X as the first letter)/(number of words)
     # chained P (x, y) = (occurences of x, y) / (number of pairs with x, z)
-    def self.probabilities(words, locale)
+
+    def self.train(text, locale)
+        words = TextPreprocessor.prepare(text)
+        first_letters, chained_letters = self.parse_text(words)
+        self.save_first(first_letters, locale)
+        self.save_chained(chained_letters, locale)
+    end
+
+    def self.parse_text(words)
+        first_letters = {} # {letter: occurences}
+        chained_letters = {} # {letters: occurences}
+
         words.each do |word|
-            FirstLetter.find_or_create_by(
-                letter: word.first,
-                locale: locale
-            ).increment!(:occurences)
+            first_letters[word.first] = (first_letters[word.first] || 0) + 1
 
             for i in 1..word.length-1
-                ChainedLetter.find_or_create_by(
-                    first_letter: word[i-1],
-                    second_letter: word[i],
-                    locale: locale
-                ).increment(:occurences)
+                key = word[i-1] + word[i]
+                chained_letters[key] = (chained_letters[key] || 0) + 1
             end
         end
 
-        FirstLetter.where(locale: locale).each do |letter|
-            letter.update(frequency: letter.occurences.to_f / words.count)
+        return first_letters, chained_letters
+    end
+
+    def self.save_first(first_letters, locale)
+        first_letters_sum = FirstLetter.where(locale: locale).sum(:occurences) + first_letters.values.reduce(:+)
+
+        first_letters.each do |letter, occurences|
+            first_letter = FirstLetter.find_or_create_by(
+                letter: letter,
+                locale: locale
+            )
+            first_letter.update(
+                occurences: first_letter.occurences + occurences,
+                frequency: (first_letter.occurences + occurences).to_f / first_letters_sum
+            )
         end
-        ChainedLetter.where(locale: locale).each do |letter|
-            letter.update(
-                frequency: letter.occurences.to_f / ChainedLetter.where(locale: locale, first_letter: letter.first_letter).count
+    end
+
+    def self.save_chained(chained_letters, locale)
+        chained_letters_sums = {}
+        chained_letters.keys.map {|k| k.to_s.first}.uniq.each do |fl|
+            chained_letters_sums[fl] = ChainedLetter.where(locale: locale, first_letter: fl).sum(:occurences) + chained_letters.select { |k,v| k.first == fl }.values.reduce(:+)
+        end
+
+        chained_letters.each do |letters, occurences|
+            chained_letter = ChainedLetter.find_or_create_by(
+                first_letter: letters.first,
+                second_letter: letters.last,
+                locale: locale
+            )
+            chained_letter.update(
+                occurences: chained_letter.occurences + occurences,
+                frequency: (chained_letter.occurences + occurences).to_f / chained_letters_sums[letters.first]
             )
         end
     end
