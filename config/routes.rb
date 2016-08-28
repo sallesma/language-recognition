4d61a71704a578e46a00d6ab4c9e4938f0a42bd4
Martin Salles <salles.martin@gmail.com>
2016-08-28 11:03:46 +0200

    Languages are root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'identifications#index'
+  root to: 'languages#index'
 
   resources :languages, only: [:index, :show, :new, :create]
   resources :identifications, only: [:index, :show, :new, :create]
